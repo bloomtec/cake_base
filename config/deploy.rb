a8449432502e53b330c95f7059a0c077cdf4a615
Julio C. Dominguez <juliodominguez@gmail.com>
2011-09-08 05:34:23 -0500

    carpeta uploads en files de webroot

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,24 +1,36 @@
-# CONFIGURATION
-set :application, 'ez.bloomweb.co'
-set :repository,  'git@github.com:bloomtec/cake_base.git'
-set :branch, 'master'
-set :scm, :git
-set :deploy_via, :remote_cache
-set :cakephp_app_path, 'app'
-set :cakephp_core_path, 'cake'
+# INITIAL CONFIGURATION
+set :application, "ez.bloomweb.co"
+set :export, :remote_cache
+set :keep_releases, 5
+set :cakephp_app_path, "app"
+set :cakephp_core_path, "cake"
+default_run_options[:pty] = true # Para pedir la contraseña de la llave publica de github via consola, sino sale error de llave publica.
 
 # DEPLOYMENT DIRECTORY STRUCTURE
-set :deploy_to, '/home/embalao/ez.bloomweb.co'
-role :app, 'ez.bloomweb.co'
-role :web, 'ez.bloomweb.co'
-role :db, 'mysql.bloomweb.co', :primary => true
+set :deploy_to, "/home/embalao/ez.bloomweb.co"
 
-# MISCELLANEOUS CONFIGURATION
-set :user, 'bloomweb'
-set :scm_username, 'rr40r900343'
+# ROLES
+role :app, "ez.bloomweb.co"
+role :web, "ez.bloomweb.co"
+role :db, "ez.bloomweb.co", :primary => true
+
+# DREAMHOST INFORMATION
+set :user, "embalao"
+
+# VERSION TRACKER INFORMATION
+set :scm, :git
 set :use_sudo, false
-
-# EXCLUDE FILES
-set :copy_exclude, ['.git', '.gitignore']
+set :repository,  "git@github.com:bloomtec/cake_base.git"
+set :branch, "master"
 
 # TASKS
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "cp /home/embalao/ez.bloomweb.co/current/. /home/embalao/ez.bloomweb.co/ -R"
+    run "chmod 777 /home/embalao/ez.bloomweb.co/app/tmp/ -R"
+    run "chmod 777 /home/embalao/ez.bloomweb.co/app/webroot/img/uploads/ -R"
+    run "chmod 777 /home/embalao/ez.bloomweb.co/app/webroot/files/uploads/ -R"
+  end
+end
