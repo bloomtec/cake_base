7ab37d71051bad1125e0516e8f44a1d4c4db097e
Julio C. Dominguez <juliodominguez@gmail.com>
2011-09-08 03:34:23 -0500

    deploy modificado con los datos de bloomweb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,11 @@
-# Configuration
-set :application, 'EZ CMS'
+# CONFIGURATION
+set :application, 'ez.bloomweb.co'
 set :repository,  'git@github.com:bloomtec/cake_base.git'
 set :branch, 'master'
 set :scm, :git
+set :deploy_via, :remote_cache
+set :cakephp_app_path, 'app'
+set :cakephp_core_path, 'cake'
 
 # DEPLOYMENT DIRECTORY STRUCTURE
 set :deploy_to, '/home/embalao/ez.bloomweb.co'
@@ -14,3 +17,8 @@ role :db, 'mysql.bloomweb.co', :primary => true
 set :user, 'bloomweb'
 set :scm_username, 'rr40r900343'
 set :use_sudo, false
+
+# EXCLUDE FILES
+set :copy_exclude, ['.git', '.gitignore']
+
+# TASKS
