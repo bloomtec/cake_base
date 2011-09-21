514f3fcef894a589f9e80e1349d9b646b7d87652
Julio C. Dominguez <juliodominguez@gmail.com>
2011-09-20 22:55:25 -0500

    cambio en el content

--- app/controllers/users_teams_controller.php
+++ app/controllers/users_teams_controller.php
@@ -121,9 +121,8 @@ class UsersTeamsController extends AppController {
 			$subject = "Convocatoria Al Equipo :: $team_name";
 			$content =
 				"<div class=\"notificacion-usuario\">"
-				. "Has sido convocado al equipo $team_name.<br />"
-				. "Para aceptar la invitación da click <a class=\"aceptar\" href='/users_teams/acceptCallToTeam/$user_id/$team_id'>aquí</a><br />"
-				. "Para rechazar la invitación da click <a class=\"rechazar\" href='/users_teams/rejectCallToTeam/$user_id/$team_id'>aquí</a><br />"
+				. "<a class=\"aceptar\" href='/users_teams/acceptCallToTeam/$user_id/$team_id'>Aceptar</a><br />"
+				. "<class=\"rechazar\" href='/users_teams/rejectCallToTeam/$user_id/$team_id'>Rechazar</a><br />"
 				. "</div>";
 			$done = $this->requestAction(
 				'/user_notifications/createNotification/'
