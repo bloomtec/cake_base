974b951e28d8e4023d0e873a0dd1084a4c5bbe97
pandales <ricardopandales@gmail.com>
2011-09-26 23:34:35 -0500

    scroll circular

--- app/webroot/js/front.js
+++ app/webroot/js/front.js
@@ -55,11 +55,11 @@ $(function(){
 					random					: 	0,		//Randomize slide order (Ignores start slide)
 					slide_interval          :   3000,	//Length between transitions
 					transition              :   1, 		//0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
-					transition_speed		:	2000,	//Speed of transition
-					new_window				:	1,		//Image links open in new window/tab
+					transition_speed		:	3000,	//Speed of transition
+					new_window				:	0,		//Image links open in new window/tab
 					pause_hover             :   0,		//Pause slideshow on hover
-					keyboard_nav            :   1,		//Keyboard navigation on/off
-					performance				:	1,		//0-Normal, 1-Hybrid speed/quality, 2-Optimizes image quality, 3-Optimizes transition speed // (Only works for Firefox/IE, not Webkit)
+					keyboard_nav            :   0,		//Keyboard navigation on/off
+					performance				:	3,		//0-Normal, 1-Hybrid speed/quality, 2-Optimizes image quality, 3-Optimizes transition speed // (Only works for Firefox/IE, not Webkit)
 					image_protect			:	1,		//Disables image dragging and right click with Javascript
 					image_path				:	'img/', //Default image path
 
@@ -76,10 +76,13 @@ $(function(){
 					thumbnail_navigation    :   0,		//Thumbnail navigation
 					slide_counter           :   0,		//Display slide numbers
 					slide_captions          :   0,		//Slide caption (Pull from "title" in slides array)
-					slides 					:  imagenes
+					slides 					:  imagenes,
+		
 												
 				}); 
 
 //SLIDE
-	$(".scrollable").scrollable();
+	$(".scrollable").scrollable({
+		circular:true;
+	});
 });
