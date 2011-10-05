a8049c83a578fd1f590ed1e8340ab422e3828c20
Juan Sebastian <juansebas0712@gmail.com>
2011-10-05 10:46:21 -0500

    prototipo

---
+++ app/webroot/prototipo/detalle_categoria_files/axurerp_pagespecificscript.js
@@ -0,0 +1,548 @@
+﻿
+var PageName = 'detalle categoria';
+var PageId = '80051dee6c8643a69e57d95d53206ecc'
+var PageUrl = 'detalle_categoria.html'
+document.title = 'detalle categoria';
+var PageNotes = 
+{
+"pageName":"detalle categoria",
+"showNotesNames":"False"}
+var $OnLoadVariable = '';
+
+var $CSUM;
+
+var hasQuery = false;
+var query = window.location.hash.substring(1);
+if (query.length > 0) hasQuery = true;
+var vars = query.split("&");
+for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split("=");
+    if (pair[0].length > 0) eval("$" + pair[0] + " = decodeURIComponent(pair[1]);");
+} 
+
+if (hasQuery && $CSUM != 1) {
+alert('Prototype Warning: The variable values were too long to pass to this page.\nIf you are using IE, using Firefox will support more data.');
+}
+
+function GetQuerystring() {
+    return '#OnLoadVariable=' + encodeURIComponent($OnLoadVariable) + '&CSUM=1';
+}
+
+function PopulateVariables(value) {
+    var d = new Date();
+  value = value.replace(/\[\[OnLoadVariable\]\]/g, $OnLoadVariable);
+  value = value.replace(/\[\[PageName\]\]/g, PageName);
+  value = value.replace(/\[\[GenDay\]\]/g, '5');
+  value = value.replace(/\[\[GenMonth\]\]/g, '10');
+  value = value.replace(/\[\[GenMonthName\]\]/g, 'octubre');
+  value = value.replace(/\[\[GenDayOfWeek\]\]/g, 'miércoles');
+  value = value.replace(/\[\[GenYear\]\]/g, '2011');
+  value = value.replace(/\[\[Day\]\]/g, d.getDate());
+  value = value.replace(/\[\[Month\]\]/g, d.getMonth() + 1);
+  value = value.replace(/\[\[MonthName\]\]/g, GetMonthString(d.getMonth()));
+  value = value.replace(/\[\[DayOfWeek\]\]/g, GetDayString(d.getDay()));
+  value = value.replace(/\[\[Year\]\]/g, d.getFullYear());
+  return value;
+}
+
+function OnLoad(e) {
+
+}
+
+var u31 = document.getElementById('u31');
+gv_vAlignTable['u31'] = 'center';
+var u36 = document.getElementById('u36');
+
+u36.style.cursor = 'pointer';
+if (bIE) u36.attachEvent("onclick", Clicku36);
+else u36.addEventListener("click", Clicku36, true);
+function Clicku36(e)
+{
+windowEvent = e;
+
+
+if (true) {
+
+	self.location.href="Home.html" + GetQuerystring();
+
+}
+
+}
+
+var u16 = document.getElementById('u16');
+
+var u17 = document.getElementById('u17');
+gv_vAlignTable['u17'] = 'top';
+var u28 = document.getElementById('u28');
+gv_vAlignTable['u28'] = 'center';
+var u29 = document.getElementById('u29');
+
+u29.style.cursor = 'pointer';
+if (bIE) u29.attachEvent("onclick", Clicku29);
+else u29.addEventListener("click", Clicku29, true);
+function Clicku29(e)
+{
+windowEvent = e;
+
+
+if (true) {
+
+	self.location.href="Home.html" + GetQuerystring();
+
+}
+
+}
+
+var u8 = document.getElementById('u8');
+
+var u30 = document.getElementById('u30');
+
+var u21 = document.getElementById('u21');
+
+var u6 = document.getElementById('u6');
+
+var u57 = document.getElementById('u57');
+
+u57.style.cursor = 'pointer';
+if (bIE) u57.attachEvent("onclick", Clicku57);
+else u57.addEventListener("click", Clicku57, true);
+function Clicku57(e)
+{
+windowEvent = e;
+
+
+if (true) {
+
+	BringToFront("u10");
+
+	SetPanelVisibility('u10','toggle','fade',500);
+
+}
+
+}
+
+var u32 = document.getElementById('u32');
+
+u32.style.cursor = 'pointer';
+if (bIE) u32.attachEvent("onclick", Clicku32);
+else u32.addEventListener("click", Clicku32, true);
+function Clicku32(e)
+{
+windowEvent = e;
+
+
+if (true) {
+
+	self.location.href="nosotros.html" + GetQuerystring();
+
+}
+
+}
+
+var u15 = document.getElementById('u15');
+
+var u13 = document.getElementById('u13');
+
+var u14 = document.getElementById('u14');
+gv_vAlignTable['u14'] = 'center';
+var u4 = document.getElementById('u4');
+gv_vAlignTable['u4'] = 'center';
+var u38 = document.getElementById('u38');
+
+var u43 = document.getElementById('u43');
+
+var u44 = document.getElementById('u44');
+
+u44.style.cursor = 'pointer';
+if (bIE) u44.attachEvent("onclick", Clicku44);
+else u44.addEventListener("click", Clicku44, true);
+function Clicku44(e)
+{
+windowEvent = e;
+
+
+if (true) {
+
+	self.location.href="Home.html" + GetQuerystring();
+
+}
+
+}
+
+var u40 = document.getElementById('u40');
+
+var u1 = document.getElementById('u1');
+
+u1.style.cursor = 'pointer';
+if (bIE) u1.attachEvent("onclick", Clicku1);
+else u1.addEventListener("click", Clicku1, true);
+function Clicku1(e)
+{
+windowEvent = e;
+
+
+if (true) {
+
+	SendToBack("u0");
+
+	SetPanelVisibility('u0','toggle','fade',500);
+
+}
+
+}
+
+var u37 = document.getElementById('u37');
+gv_vAlignTable['u37'] = 'center';
+var u26 = document.getElementById('u26');
+gv_vAlignTable['u26'] = 'center';
+var u41 = document.getElementById('u41');
+
+var u10 = document.getElementById('u10');
+
+var u11 = document.getElementById('u11');
+
+u11.style.cursor = 'pointer';
+if (bIE) u11.attachEvent("onclick", Clicku11);
+else u11.addEventListener("click", Clicku11, true);
+function Clicku11(e)
+{
+windowEvent = e;
+
+
+if (true) {
+
+	BringToFront("u10");
+
+	SetPanelVisibility('u10','toggle','fade',500);
+
+}
+
+}
+
+var u3 = document.getElementById('u3');
+
+var u12 = document.getElementById('u12');
+gv_vAlignTable['u12'] = 'center';
+var u39 = document.getElementById('u39');
+gv_vAlignTable['u39'] = 'center';
+var u9 = document.getElementById('u9');
+gv_vAlignTable['u9'] = 'top';
+var u35 = document.getElementById('u35');
+
+u35.style.cursor = 'pointer';
+if (bIE) u35.attachEvent("onclick", Clicku35);
+else u35.addEventListener("click", Clicku35, true);
+function Clicku35(e)
+{
+windowEvent = e;
+
+
+if (true) {
+
+	BringToFront("u24");
+
+	SetPanelVisibility('u24','toggle','fade',500);
+
+}
+
+}
+
+var u27 = document.getElementById('u27');
+
+var u7 = document.getElementById('u7');
+
+var u42 = document.getElementById('u42');
+gv_vAlignTable['u42'] = 'center';
+var u58 = document.getElementById('u58');
+
+u58.style.cursor = 'pointer';
+if (bIE) u58.attachEvent("onclick", Clicku58);
+else u58.addEventListener("click", Clicku58, true);
+function Clicku58(e)
+{
+windowEvent = e;
+
+
+if (true) {
+
+	BringToFront("u0");
+
+	SetPanelVisibility('u0','','fade',500);
+
+}
+
+}
+
+var u23 = document.getElementById('u23');
+
+u23.style.cursor = 'pointer';
+if (bIE) u23.attachEvent("onclick", Clicku23);
+else u23.addEventListener("click", Clicku23, true);
+function Clicku23(e)
+{
+windowEvent = e;
+
+
+if (true) {
+
+	self.location.href="Home.html" + GetQuerystring();
+
+}
+
+}
+
+var u24 = document.getElementById('u24');
+
+var u25 = document.getElementById('u25');
+
+u25.style.cursor = 'pointer';
+if (bIE) u25.attachEvent("onclick", Clicku25);
+else u25.addEventListener("click", Clicku25, true);
+function Clicku25(e)
+{
+windowEvent = e;
+
+
+if (true) {
+
+	SetPanelVisibility('u24','hidden','fade',500);
+
+	SendToBack("u24");
+
+}
+
+}
+
+var u46 = document.getElementById('u46');
+
+u46.style.cursor = 'pointer';
+if (bIE) u46.attachEvent("onclick", Clicku46);
+else u46.addEventListener("click", Clicku46, true);
+function Clicku46(e)
+{
+windowEvent = e;
+
+
+if (true) {
+
+	NewTab("http://www.twitter.com" + "", "");
+
+}
+
+}
+
+var u53 = document.getElementById('u53');
+
+u53.style.cursor = 'pointer';
+if (bIE) u53.attachEvent("onclick", Clicku53);
+else u53.addEventListener("click", Clicku53, true);
+function Clicku53(e)
+{
+windowEvent = e;
+
+
+if (true) {
+
+	self.location.href="carrito_de_compras.html" + GetQuerystring();
+
+}
+
+}
+
+var u56 = document.getElementById('u56');
+
+u56.style.cursor = 'pointer';
+if (bIE) u56.attachEvent("onclick", Clicku56);
+else u56.addEventListener("click", Clicku56, true);
+function Clicku56(e)
+{
+windowEvent = e;
+
+
+if (true) {
+
+	BringToFront("u18");
+
+	SetPanelVisibility('u18','toggle','fade',500);
+
+}
+
+}
+
+var u54 = document.getElementById('u54');
+
+u54.style.cursor = 'pointer';
+if (bIE) u54.attachEvent("onclick", Clicku54);
+else u54.addEventListener("click", Clicku54, true);
+function Clicku54(e)
+{
+windowEvent = e;
+
+
+if (true) {
+
+	SetPanelVisibility('u48','','fade',500);
+function waitu7659b12eee504a0e8704b21c08b3bb151() {
+
+	SetPanelVisibility('u48','hidden','fade',500);
+}
+setTimeout(waitu7659b12eee504a0e8704b21c08b3bb151, 4000);
+
+}
+
+}
+
+var u2 = document.getElementById('u2');
+gv_vAlignTable['u2'] = 'center';
+var u18 = document.getElementById('u18');
+
+var u19 = document.getElementById('u19');
+
+u19.style.cursor = 'pointer';
+if (bIE) u19.attachEvent("onclick", Clicku19);
+else u19.addEventListener("click", Clicku19, true);
+function Clicku19(e)
+{
+windowEvent = e;
+
+
+if (true) {
+
+	BringToFront("u18");
+
+	SetPanelVisibility('u18','toggle','fade',500);
+
+}
+
+}
+
+var u55 = document.getElementById('u55');
+
+u55.style.cursor = 'pointer';
+if (bIE) u55.attachEvent("onclick", Clicku55);
+else u55.addEventListener("click", Clicku55, true);
+function Clicku55(e)
+{
+windowEvent = e;
+
+
+if (true) {
+
+	SetPanelVisibility('u51','','fade',500);
+function waitu548a4e1882994c69b978a612d0a71c331() {
+
+	SetPanelVisibility('u51','hidden','fade',500);
+}
+setTimeout(waitu548a4e1882994c69b978a612d0a71c331, 4000);
+
+}
+
+}
+
+var u20 = document.getElementById('u20');
+gv_vAlignTable['u20'] = 'center';
+var u5 = document.getElementById('u5');
+
+var u48 = document.getElementById('u48');
+
+var u22 = document.getElementById('u22');
+gv_vAlignTable['u22'] = 'center';
+var u49 = document.getElementById('u49');
+gv_vAlignTable['u49'] = 'top';
+var u47 = document.getElementById('u47');
+
+u47.style.cursor = 'pointer';
+if (bIE) u47.attachEvent("onclick", Clicku47);
+else u47.addEventListener("click", Clicku47, true);
+function Clicku47(e)
+{
+windowEvent = e;
+
+
+if (true) {
+
+	self.location.href="carrito_de_compras.html" + GetQuerystring();
+
+}
+
+}
+
+var u50 = document.getElementById('u50');
+
+u50.style.cursor = 'pointer';
+if (bIE) u50.attachEvent("onclick", Clicku50);
+else u50.addEventListener("click", Clicku50, true);
+function Clicku50(e)
+{
+windowEvent = e;
+
+
+if (true) {
+
+	self.location.href="carrito_de_compras.html" + GetQuerystring();
+
+}
+
+}
+
+var u51 = document.getElementById('u51');
+
+var u45 = document.getElementById('u45');
+
+u45.style.cursor = 'pointer';
+if (bIE) u45.attachEvent("onclick", Clicku45);
+else u45.addEventListener("click", Clicku45, true);
+function Clicku45(e)
+{
+windowEvent = e;
+
+
+if (true) {
+
+	NewTab("http://www.facebook.com" + "", "");
+
+}
+
+}
+
+var u52 = document.getElementById('u52');
+gv_vAlignTable['u52'] = 'top';
+var u33 = document.getElementById('u33');
+
+u33.style.cursor = 'pointer';
+if (bIE) u33.attachEvent("onclick", Clicku33);
+else u33.addEventListener("click", Clicku33, true);
+function Clicku33(e)
+{
+windowEvent = e;
+
+
+if (true) {
+
+	self.location.href="contacto.html" + GetQuerystring();
+
+}
+
+}
+
+var u34 = document.getElementById('u34');
+
+u34.style.cursor = 'pointer';
+if (bIE) u34.attachEvent("onclick", Clicku34);
+else u34.addEventListener("click", Clicku34, true);
+function Clicku34(e)
+{
+windowEvent = e;
+
+
+if (true) {
+
+	self.location.href="ayuda.html" + GetQuerystring();
+
+}
+
+}
+
+var u0 = document.getElementById('u0');
+
+if (window.OnLoad) OnLoad();
---
+++ app/webroot/prototipo/notificarme_cuendo_este_disponible_files/axurerp_pagespecificscript.js
@@ -0,0 +1,76 @@
+﻿
+var PageName = 'notificarme cuendo este disponible';
+var PageId = '1a35ac87c5ca402d8b1ac91f46db5ff6'
+var PageUrl = 'notificarme_cuendo_este_disponible.html'
+document.title = 'notificarme cuendo este disponible';
+var PageNotes = 
+{
+"pageName":"notificarme cuendo este disponible",
+"showNotesNames":"False"}
+var $OnLoadVariable = '';
+
+var $CSUM;
+
+var hasQuery = false;
+var query = window.location.hash.substring(1);
+if (query.length > 0) hasQuery = true;
+var vars = query.split("&");
+for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split("=");
+    if (pair[0].length > 0) eval("$" + pair[0] + " = decodeURIComponent(pair[1]);");
+} 
+
+if (hasQuery && $CSUM != 1) {
+alert('Prototype Warning: The variable values were too long to pass to this page.\nIf you are using IE, using Firefox will support more data.');
+}
+
+function GetQuerystring() {
+    return '#OnLoadVariable=' + encodeURIComponent($OnLoadVariable) + '&CSUM=1';
+}
+
+function PopulateVariables(value) {
+    var d = new Date();
+  value = value.replace(/\[\[OnLoadVariable\]\]/g, $OnLoadVariable);
+  value = value.replace(/\[\[PageName\]\]/g, PageName);
+  value = value.replace(/\[\[GenDay\]\]/g, '5');
+  value = value.replace(/\[\[GenMonth\]\]/g, '10');
+  value = value.replace(/\[\[GenMonthName\]\]/g, 'octubre');
+  value = value.replace(/\[\[GenDayOfWeek\]\]/g, 'miércoles');
+  value = value.replace(/\[\[GenYear\]\]/g, '2011');
+  value = value.replace(/\[\[Day\]\]/g, d.getDate());
+  value = value.replace(/\[\[Month\]\]/g, d.getMonth() + 1);
+  value = value.replace(/\[\[MonthName\]\]/g, GetMonthString(d.getMonth()));
+  value = value.replace(/\[\[DayOfWeek\]\]/g, GetDayString(d.getDay()));
+  value = value.replace(/\[\[Year\]\]/g, d.getFullYear());
+  return value;
+}
+
+function OnLoad(e) {
+
+}
+
+var u3 = document.getElementById('u3');
+
+u3.style.cursor = 'pointer';
+if (bIE) u3.attachEvent("onclick", Clicku3);
+else u3.addEventListener("click", Clicku3, true);
+function Clicku3(e)
+{
+windowEvent = e;
+
+
+if (true) {
+
+	self.location.href="Home.html" + GetQuerystring();
+
+}
+
+}
+
+var u0 = document.getElementById('u0');
+
+var u1 = document.getElementById('u1');
+gv_vAlignTable['u1'] = 'center';
+var u2 = document.getElementById('u2');
+
+if (window.OnLoad) OnLoad();
