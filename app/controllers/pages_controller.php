9d337a9fc3fa0a6d7b0039d90d360a00a5b8369f
Juan Sebastian <juansebas0712@gmail.com>
2011-10-05 17:02:55 -0500

    carrito de compras

--- app/controllers/pages_controller.php
+++ app/controllers/pages_controller.php
@@ -24,6 +24,9 @@ class PagesController extends AppController {
 		$this->set(compact('page', 'subpage', 'title_for_layout'));
 		$this->render(implode('/', $path));
 	}
+	function shop_cart(){
+		$this->layout="carrito";
+	}
 	function ayuda(){
 		$this->layout="pages";
 	}
---
+++ app/views/elements/menu-shop-cart.ctp
@@ -0,0 +1,7 @@
+<ul class="menu_carrito twCenMt">
+	<li><a class="azul" href="#">CARRITO DE COMPRAS</a></li>
+	<li><a href="#">ENVÍO</a></li>
+	<li><a href="#">MÉTODO DE ENVIO</a></li>
+	<li><a href="#">PAGO</a></li>
+</ul>
+<div style="clear: both"></div>
