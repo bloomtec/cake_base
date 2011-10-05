e4250f616636cceae931bb1b6be83a6117db9c12
Ricardo Pandales <ricardopandales@gmail.com>
2011-10-05 11:34:49 -0500

    capified

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # INITIAL CONFIGURATION
-set :application, "ez.bloomweb.co"
+set :application, "colors.bloomweb.co"
 set :export, :remote_cache
 set :keep_releases, 5
 set :cakephp_app_path, "app"
@@ -7,12 +7,12 @@ set :cakephp_core_path, "cake"
 default_run_options[:pty] = true # Para pedir la contraseña de la llave publica de github via consola, sino sale error de llave publica.
 
 # DEPLOYMENT DIRECTORY STRUCTURE
-set :deploy_to, "/home/embalao/ez.bloomweb.co"
+set :deploy_to, "/home/embalao/colors.bloomweb.co"
 
 # ROLES
-role :app, "ez.bloomweb.co"
-role :web, "ez.bloomweb.co"
-role :db, "ez.bloomweb.co", :primary => true
+role :app, "colors.bloomweb.co"
+role :web, "colors.bloomweb.co"
+role :db, "colors.bloomweb.co", :primary => true
 
 # DREAMHOST INFORMATION
 set :user, "embalao"
@@ -20,7 +20,7 @@ set :user, "embalao"
 # VERSION TRACKER INFORMATION
 set :scm, :git
 set :use_sudo, false
-set :repository,  "git@github.com:bloomtec/cake_base.git"
+set :repository,  "git@github.com:bloomtec/colors.git"
 set :branch, "master"
 
 # TASKS
@@ -28,9 +28,9 @@ namespace :deploy do
   task :start do ; end
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
-    run "cp /home/embalao/ez.bloomweb.co/current/. /home/embalao/ez.bloomweb.co/ -R"
-    run "chmod 777 /home/embalao/ez.bloomweb.co/app/tmp/ -R"
-    run "chmod 777 /home/embalao/ez.bloomweb.co/app/webroot/img/uploads/ -R"
-    run "chmod 777 /home/embalao/ez.bloomweb.co/app/webroot/files/uploads/ -R"
+    run "cp /home/embalao/colors.bloomweb.co/current/. /home/embalao/colors.bloomweb.co/ -R"
+    run "chmod 777 /home/embalao/colors.bloomweb.co/app/tmp/ -R"
+    run "chmod 777 /home/embalao/colors.bloomweb.co/app/webroot/img/uploads/ -R"
+    run "chmod 777 /home/embalao/colors.bloomweb.co/app/webroot/files/uploads/ -R"
   end
 end
