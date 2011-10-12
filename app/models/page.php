51501ae9a00e1649abaa16fada6138df17d5c14e
Ricardo Pandales <ricardopandales@gmail.com>
2011-10-12 17:16:57 -0500

    modelo y controlador de pages; register y validaciones incluidas

---
+++ app/models/page.php
@@ -0,0 +1,31 @@
+<?php
+class Page extends AppModel {
+	var $name = 'Page';
+	var $displayField = 'name';
+	var $validate = array(
+		'name' => array(
+			'notempty' => array(
+				'rule' => array(
+					'notempty'
+				),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'slug' => array(
+			'notempty' => array(
+				'rule' => array(
+					'notempty'
+				),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+	);
+}
