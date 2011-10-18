0822229e9f267cee0150d73da075f2b896439b0b
panda <ricardopandales@gmail.com>
2011-10-18 16:00:21 -0500

    nuevos controladores

---
+++ app/controllers/visited_products_controller.php
@@ -0,0 +1,155 @@
+<?php
+class VisitedProductsController extends AppController {
+
+	var $name = 'VisitedProducts';
+
+	function index() {
+		$this->VisitedProduct->recursive = 0;
+		$this->set('visitedProducts', $this->paginate());
+	}
+	
+	function view($id = null) {
+		if (!$id) {
+			$this->Session->setFlash(__('Invalid visited product', true));
+			$this->redirect(array('action' => 'index'));
+		}
+		$this->set('visitedProduct', $this->VisitedProduct->read(null, $id));
+	}
+
+
+	function setInactive($id = null) {
+		if (!$id) {
+			$this->Session->setFlash(__('Invalid id for visited product', true));
+			$this->redirect(array('action'=>'index'));
+		}
+		$oldData=$this->VisitedProduct->read(null,$id);
+		$oldData["VisitedProduct"]["active"]=false;
+		if ($this->VisitedProduct->save($oldData)) {
+			$this->Session->setFlash(__('Visited product archived', true));
+			$this->redirect(array('action'=>'index'));
+		}
+		$this->Session->setFlash(__('Visited product was not archived', true));
+		$this->redirect(array('action' => 'index'));
+	}
+function setActive($id = null) {
+		if (!$id) {
+			$this->Session->setFlash(__('Invalid id for visited product', true));
+			$this->redirect(array('action'=>'index'));
+		}
+		$oldData=$this->VisitedProduct->read(null,$id);
+		$oldData["VisitedProduct"]["active"]=true;
+		if ($this->VisitedProduct->save($oldData)) {
+			$this->Session->setFlash(__('Visited product archived', true));
+			$this->redirect(array('action'=>'index'));
+		}
+		$this->Session->setFlash(__('Visited product was not archived', true));
+		$this->redirect(array('action' => 'index'));
+	}
+function requestFind($type,$findParams,$key) {
+	if($key==Configure::read("key")){
+		return $this->VisitedProduct->find($type, $findParams);
+	}else{
+		return null;
+	}
+}
+	function admin_index() {
+		$this->VisitedProduct->recursive = 0;
+		$this->set('visitedProducts', $this->paginate());
+	}
+
+	function admin_view($id = null) {
+		if (!$id) {
+			$this->Session->setFlash(__('Invalid visited product', true));
+			$this->redirect(array('action' => 'index'));
+		}
+		$this->set('visitedProduct', $this->VisitedProduct->read(null, $id));
+	}
+
+	function admin_add() {
+		if (!empty($this->data)) {
+			$this->VisitedProduct->create();
+			if ($this->VisitedProduct->save($this->data)) {
+				$this->Session->setFlash(__('The visited product has been saved', true));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The visited product could not be saved. Please, try again.', true));
+			}
+		}
+		$products = $this->VisitedProduct->Product->find('list');
+		$users = $this->VisitedProduct->User->find('list');
+		$this->set(compact('products', 'users'));
+	}
+
+	function admin_edit($id = null) {
+		if (!$id && empty($this->data)) {
+			$this->Session->setFlash(__('Invalid visited product', true));
+			$this->redirect(array('action' => 'index'));
+		}
+		if (!empty($this->data)) {
+			if ($this->VisitedProduct->save($this->data)) {
+				$this->Session->setFlash(__('The visited product has been saved', true));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The visited product could not be saved. Please, try again.', true));
+			}
+		}
+		if (empty($this->data)) {
+			$this->data = $this->VisitedProduct->read(null, $id);
+		}
+		$products = $this->VisitedProduct->Product->find('list');
+		$users = $this->VisitedProduct->User->find('list');
+		$this->set(compact('products', 'users'));
+	}
+
+	function admin_delete($id = null) {
+		if (!$id) {
+			$this->Session->setFlash(__('Invalid id for visited product', true));
+			$this->redirect(array('action'=>'index'));
+		}
+		if ($this->VisitedProduct->delete($id)) {
+			$this->Session->setFlash(__('Visited product deleted', true));
+			$this->redirect(array('action'=>'index'));
+		}
+		$this->Session->setFlash(__('Visited product was not deleted', true));
+		$this->redirect(array('action' => 'index'));
+	}
+
+
+
+
+	function admin_setInactive($id = null) {
+		if (!$id) {
+			$this->Session->setFlash(__('Invalid id for visited product', true));
+			$this->redirect(array('action'=>'index'));
+		}
+		$oldData=$this->VisitedProduct->read(null,$id);
+		$oldData["VisitedProduct"]["active"]=false;
+		if ($this->VisitedProduct->save($oldData)) {
+			$this->Session->setFlash(__('Visited product archived', true));
+			$this->redirect(array('action'=>'index'));
+		}
+		$this->Session->setFlash(__('Visited product was not archived', true));
+		$this->redirect(array('action' => 'index'));
+	}
+function admin_setActive($id = null) {
+		if (!$id) {
+			$this->Session->setFlash(__('Invalid id for visited product', true));
+			$this->redirect(array('action'=>'index'));
+		}
+		$oldData=$this->VisitedProduct->read(null,$id);
+		$oldData["VisitedProduct"]["active"]=true;
+		if ($this->VisitedProduct->save($oldData)) {
+			$this->Session->setFlash(__('Visited product archived', true));
+			$this->redirect(array('action'=>'index'));
+		}
+		$this->Session->setFlash(__('Visited product was not archived', true));
+		$this->redirect(array('action' => 'index'));
+	}
+function admin_requestFind($type,$findParams,$key) {
+	if($key==Configure::read("key")){
+		return $this->VisitedProduct->find($type, $findParams);
+	}else{
+		return null;
+	}
+}
+}
---
+++ app/models/visited_product.php
@@ -0,0 +1,22 @@
+<?php
+class VisitedProduct extends AppModel {
+	var $name = 'VisitedProduct';
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+	var $belongsTo = array(
+		'Product' => array(
+			'className' => 'Product',
+			'foreignKey' => 'product_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		),
+		'User' => array(
+			'className' => 'User',
+			'foreignKey' => 'user_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
+}
