cb40135c1ecb7d757af255b604f77af0072519d1
Julio César Domínguez Giraldo <jucedogi@ubuntu-vm.(none)>
2011-10-18 00:25:54 -0500

    revision navegacion de cupones

--- app/controllers/coupons_controller.php
+++ app/controllers/coupons_controller.php
@@ -11,14 +11,15 @@ class CouponsController extends AppController {
 	function admin_delete($id = null) {
 		if (!$id) {
 			$this -> Session -> setFlash(__('Invalid id for coupon', true));
-			$this -> redirect(array('action' => 'index'));
+			$this -> redirect(array('controller' => 'coupon_batches', 'action' => 'index'));
 		}
+		$coupon = $this->Coupon->read(null, $id);
 		if ($this -> Coupon -> delete($id)) {
 			$this -> Session -> setFlash(__('Coupon deleted', true));
-			$this -> redirect(array('action' => 'index'));
+			$this -> redirect(array('controller' => 'coupon_batches', 'action' => 'view', $coupon['Coupon']['coupon_batch_id']));
 		}
 		$this -> Session -> setFlash(__('Coupon was not deleted', true));
-		$this -> redirect(array('action' => 'index'));
+		$this -> redirect(array('controller' => 'coupon_batches', 'action' => 'view', $coupon['Coupon']['coupon_batch_id']));
 	}
 
 }
