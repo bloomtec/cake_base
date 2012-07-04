2641d3dfe1af59af281ef64aa8c1665f02ded58d
Julio César Domínguez Giraldo <jucedogi@ubuntu-vb.(none)>
2012-07-03 23:09:35 -0500

    revisiones correo registro

--- app/controllers/users_controller.php
+++ app/controllers/users_controller.php
@@ -554,7 +554,6 @@ class UsersController extends AppController {
 	}
 
 	function validateEmail($code = null) {
-	/*
 		if (!$code && !empty($this -> data)) {
 			if (isset($this -> data['User']['validation_code']) && !empty($this -> data['User']['validation_code'])) {
 				$code = $this -> data['User']['validation_code'];
@@ -593,7 +592,6 @@ class UsersController extends AppController {
 		} else {
 			$this -> Session -> setFlash(__('Ingresa un código válido e intenta de nuevo.', true));
 		}
-		*/
 	}
 
 	function profile() {
