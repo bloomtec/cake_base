98b9f8d304578878c862f00c31056440f438fbb8
Julio César Domínguez <jucedogi@ubuntu-vm.(none)>
2012-08-10 15:27:57 -0500

    revs

--- app/controllers/orders_controller.php
+++ app/controllers/orders_controller.php
@@ -61,6 +61,59 @@ class OrdersController extends AppController {
 
 	}
 	
+	private function orderSentEmail($user_id = null, $order_code = null) {
+		/**
+		 * Asignar las variables del componente Email
+		 */
+		if ($user_id && $comments && $order_code) {
+			$user = $this -> Order -> User -> read(null, $user_id);
+			
+			// Address the message is going to (string). Separate the addresses with a comma if you want to send the email to more than one recipient.
+			$this -> Email -> to = $user['User']['email'];
+			// array of addresses to cc the message to
+			$this -> Email -> cc = '';
+			// array of addresses to bcc (blind carbon copy) the message to
+			$this -> Email -> bcc = '';
+			// reply to address (string)
+			$this -> Email -> replyTo = Configure::read('info_mail');
+			// Return mail address that will be used in case of any errors(string) (for mail-daemon/errors)
+			$this -> Email -> return = Configure::read('reply_info_mail');
+			// from address (string)
+			$this -> Email -> from = Configure::read('info_mail');
+			// subject for the message (string)
+			$this -> Email -> subject = __('Orden despachada :: ', true) . Configure::read('site_name');
+			// The email element to use for the message (located in app/views/elements/email/html/ and app/views/elements/email/text/)
+			$this -> Email -> template = 'order_sent_email';
+			// The layout used for the email (located in app/views/layouts/email/html/ and app/views/layouts/email/text/)
+			//$this -> Email -> layout = '';
+			// Length at which lines should be wrapped. Defaults to 70. (integer)
+			//$this -> Email -> lineLength = '';
+			// how do you want message sent string values of text, html or both
+			$this -> Email -> sendAs = 'html';
+			// array of files to send (absolute and relative paths)
+			//$this -> Email -> attachments = '';
+			// how to send the message (mail, smtp [would require smtpOptions set below] and debug)
+			$this -> Email -> delivery = 'smtp';
+			// associative array of options for smtp mailer (port, host, timeout, username, password, client)
+			$this -> Email -> smtpOptions = array('port' => '465', 'timeout' => '30', 'host' => 'ssl://smtp.gmail.com', 'username' => Configure::read('info_mail'), 'password' => Configure::read('password_info_mail'), 'client' => 'smtp_helo_comopromos.com');
+
+			/**
+			 * Asignar cosas al template
+			 */
+			$this -> set('user_full_name', $user['User']['name'] . ' ' . $user['User']['last_name']);
+			$this -> set('order_code', $order_code);
+
+			/**
+			 * Enviar el correo
+			 */
+			Configure::write('debug', 0);
+			$this -> Email -> send();
+			$this -> set('smtp_errors', $this -> Email -> smtpError);
+			$this -> Email -> reset();
+		}
+
+	}
+	
 	function changeStatus($id,$newState,$comments=null){
 		// debe validar tambien que la persona autenticada es la duela del restaurante de la orden de la promocion
 		//devuelve true o false
@@ -78,6 +131,9 @@ class OrdersController extends AppController {
 					$order['Order']['rejection_note']=mysql_real_escape_string($comments);
 				}
 				if($this -> Order -> save($order)){
+					if($newState==2){//ORDEN DESPACHADA
+						$this -> orderSentEmail($order['User']['id'], $order['Order']['code']);
+					}
 					if($newState==5){//APROBO LA PROMOCION
 						$this -> approve($id);
 					}
---
+++ app/views/elements/email/html/order_sent_email.ctp
@@ -0,0 +1,11 @@
+<div>
+	<p>
+		<?php echo $user_full_name . __(', te informamos que la orden con código ', true) . $order_code . __(' del sitio ', true) . Configure::read('site_name') . __(' ha sido despachada.', true); ?>
+	</p>
+	<p>
+		<?php echo __('Esperamos que uses nuestro servicio nuevamente en un futuro.', true); ?> 
+	</p>
+	<p>
+		<?php echo __('ComoPromos, Todas las promociones de comida a domicilio de tu ciudad, en un solo lugar!', true); ?>
+	</p>
+</div>
